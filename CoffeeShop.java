33962364432bf7e29b2f1940776bb19873f41cdc
Gwynyn Hayes <gwynyn_hayes@m018131.local>
2020-09-14 14:32:57 -0700

    Coffee

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -4,14 +4,25 @@ public class CoffeeShop {
 
 public static void main(String[] args) {
 
-System.out.println("");
-System.out.println("Welcome to the Coffee Shop");
-System.out.println("");
+  System.out.println("");
+  System.out.println("Welcome to the Cafe of Coffee. What can I get for you my dude?");
+  System.out.println("");
 
-Scanner userIn = new Scanner(System.in)
+Scanner orderIn = new Scanner(System.in);
+  String order = orderIn.nextLine();
+    System.out.println("To reiterate you would like " + order);
+
+System.out.println("Can I get a name for that order?");
+
+Scanner userIn = new Scanner(System.in);
 String name = userIn.next();
 
-System.out.println("Is your nam," + name);
+System.out.println("For " + name + "?");
+System.out.println("Just a moment please");
+System.out.println("...one moment later...");
+
+System.out.println("I have a " + order + " for " + name);
+System.out.println("Thank you for making an apperence at the Cafe of Coffee. Come again soon my dude!");
 }
 
 }
