4a438978ed29bdc77df58b92e469518d8df546ca
Gwynyn Hayes <gwynyn_hayes@m018131.local>
2020-10-27 14:26:42 -0700

    halloween

---
+++ Halloween.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+  public class Halloween {
+
+
+    public Halloween() {
+
+    }
+    public static void main(String[] args) {
+
+      Random R = new Random();
+      float[][] neighborhood = new float[5][5];
+
+      int bestCol1 = 0;
+      int bestRow1 = 0;
+      int bestHouse1 = 0;
+
+      int Col2 = 0;
+      int Row2 = 0;
+      int House2 = 0;
+
+      int Col3 = 0;
+      int Row3 = 0;
+      int House3 = 0;
+
+      int Col4 = 0;
+      int Row4 = 0;
+      int House4 = 0;
+
+      int Col5 = 0;
+      int Row5 = 0;
+      int House5 = 0;
+
+      for(int h = 0; x < 5; h++);
+
+      for(int g = 0; g < 5; g++);
+
+      neighborhood[h][g] = (Math.random())*10;
+
+
+
+      }
+    }
+
+
+//if statements
